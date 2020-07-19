8304af0dae2ff901bc03045298003919a9a0e7f0
cacheiforindicium <30838954+cacheiforindicium@users.noreply.github.com>
2020-07-19 15:29:30 +0000

    Create style.cs

---
+++ .config/wlogout/style.cs
@@ -0,0 +1,53 @@
+* {
+    background-image: none;
+}
+window {
+    background-image: image(url("/home/junis/.config/wallpapers/min.jpg"), url("/home/junis/.config/wallpapers/min.jpg"));
+}
+button {
+    background-color: #2e3440;
+    border-width: 0px;
+    border-radius: 0;
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 25%;
+    color: #5e81ac;
+}
+
+button:active, button:hover {
+    background-color: #4c566a;
+    color: #81a1c1;
+    outline-style: none;
+}
+
+#lock {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/logout.png"), url("/usr/share/wlogout/icons/logout.png"));
+}
+
+#logout {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/logout.png"), url("/usr/local/share/wlogout/icons/logout.png"));
+}
+
+#suspend {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/suspend.png"), url("/usr/local/share/wlogout/icons/suspend.png"));
+}
+
+#hibernate {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/hibernate.png"), url("/usr/local/share/wlogout/icons/hibernate.png"));
+}
+
+#shutdown {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/shutdown.png"), url("/usr/local/share/wlogout/icons/shutdown.png"));
+}
+
+#reboot {
+    opacity: 0.9;
+    background-image: image(url("/usr/share/wlogout/icons/reboot.png"), url("/usr/local/share/wlogout/icons/reboot.png"));
+}
+
+
